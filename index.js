f69a5a405a7cc490a85c9d66dff24127a21838fd
chiman.song <chiman.song@kurlycorp.com>
2023-05-30 17:42:26 +0900

    rediction added

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ server.get('/about', function(request, response) {
 });
 
 server.get('/goods*', function(request, response) {
-  response.sendFile(__dirname +  '/goods.html');
+  response.redirect('/goods.html');
 });
 
 server.get('/collections*', function(request, response) {
