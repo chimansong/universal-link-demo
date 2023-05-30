46a393315f8f8c4478d2db9a28c63007a1d8ec0d
chiman.song <chiman.song@kurlycorp.com>
2023-05-30 17:26:46 +0900

    rediction added

--- index.js
+++ index.js
@@ -13,6 +13,19 @@ server.get('/home', function(request, response) {
 server.get('/about', function(request, response) {
   response.sendFile(__dirname +  '/about.html');
 });
+
+server.get('/goods', function(request, response) {
+  response.sendFile(__dirname +  '/about.html');
+});
+
+server.get('/collections', function(request, response) {
+  response.sendFile(__dirname +  '/collections.html');
+});
+
+server.get('/categories', function(request, response) {
+  response.sendFile(__dirname +  '/categories.html');
+});
+
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
